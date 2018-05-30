0e373b43b077c181076e01d47f782c2862e5da43
Yangqi Li <jrs930805@gmail.com>
2018-05-30 23:57:19 +0800

    retry when error occurs
    if there is an error and the error.code belongs to [500,600), then try another two times.

--- codes/start.py
+++ codes/start.py
@@ -1,10 +1,16 @@
 import urllib2
 
-def download(url):
+def download(url,num_retries = 2):
     print 'Downloading:',url
     try:
         html = urllib2.urlopen(url).read()
     except urllib2.URLError as e:
         print 'Download erro:',e.reason
         html = None
+        if num_retries>0:
+            if hasattr(e,'code') and 500<= e.code < 600:
+                return download(url,num_retries-1)
     return html
+'''
+download('http://example.webscraping.com')
+'''
