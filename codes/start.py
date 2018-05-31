de1cbc9c800803a6986a613a32c8668811656ec5
Yangqi Li <jrs930805@gmail.com>
2018-05-31 23:53:37 +0800

    two types of scrapy
    through sitemap and ID

--- codes/start.py
+++ codes/start.py
@@ -1,4 +1,6 @@
 import urllib2
+import re
+import itertools
 
 def download(url,user_agent = 'jrs',num_retries = 2):
     print 'Downloading:',url
@@ -13,6 +15,27 @@ def download(url,user_agent = 'jrs',num_retries = 2):
             if hasattr(e,'code') and 500<= e.code < 600:
                 return download(url,num_retries-1)
     return html
+
+def crawl_sitemap(url):
+    sitemap = download(url)
+    links = re.findall('<loc>(.*?)</loc>',sitemap)
+    for link in links:
+        html = download(link)
+
+#maxinum unmber of consecutive download errors allowed
+max_errors = 5
+#current number of consecutive download errors
+num_errors = 0
+for page in itertools.count(1,1):
+    url = 'http://example.webscraping.com/view/-%d' % page
+    html = download(url)
+    if html is None:
+        num_errors +=1
+        if num_errors == max_errors:
+            break
+    else:
+        num_errors = 0
 '''
 download('http://example.webscraping.com')
+crawl_sitemap('http://example.webscraping.com/sitemap.xml')
 '''
