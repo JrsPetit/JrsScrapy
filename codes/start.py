558c898bd08088ec29befb7565dbcd6c76b6a64e
Yangqi Li <jrs930805@gmail.com>
2018-06-05 23:06:49 +0800

    check_robots
    ask for the permission of  robots.txt

--- codes/start.py
+++ codes/start.py
@@ -2,6 +2,7 @@ import urllib2
 import re
 import itertools
 import urlparse
+import robotparser
 
 def download(url,user_agent = 'jrs',num_retries = 2):
     print 'Downloading:',url
@@ -20,28 +21,43 @@ def download(url,user_agent = 'jrs',num_retries = 2):
 def link_crawler(seed_url, link_regex):
     """crawl from the given seed URL following links matched by link_regex
     """
+    rp = robotparser.RobotFileParser()
+    rp.set_url('http://192.168.1.5:8000/places/static/robots.txt')
+    rp.read()
+    user_agent = 'BadCrawler'
+
     crawl_queue = [seed_url]
     seen = set(crawl_queue)
     while crawl_queue:
         url = crawl_queue.pop()
-        html = download(url)
-        links = get_links(html)
-        for link in links:
-            if re.match(link_regex ,link):
-                link = urlparse.urljoin(seed_url, link)
-                if link not in seen:
-                    seen.add(link)
-                    crawl_queue.append(link)
-            else:
-                print 'do not want ',link
+        if rp.can_fetch(user_agent, url):
+            html = download(url)
+            links = get_links(html)
+            for link in links:
+                if re.match(link_regex ,link):
+                    link = urlparse.urljoin(seed_url, link)
+                    if link not in seen:
+                        seen.add(link)
+                        crawl_queue.append(link)
+                else:
+                    print 'do not want ',link
+        else:
+            print 'Blocked by robots.txt ',url 
 
 def get_links(html):
     print 1111
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
     return webpage_regex.findall(html)
 
-link_crawler('http://127.0.0.1:8000/places', '/places/default/(index|view)')
-
+link_crawler('http://192.168.1.5:8000/places', '/places/default/(index|view)')
+'''
+rp = robotparser.RobotFileParser()
+rp.set_url('http://example.webscraping.com/robots.txt')
+rp.read()
+url = 'http://example.webscraping.com'
+user_agent = 'jrs'
+print rp.can_fetch(user_agent,url)
+'''
 '''test_str = '/places/default/index'
 test_regex = '/places/default/(index|view)' 
 if re.match(test_regex ,test_str):
