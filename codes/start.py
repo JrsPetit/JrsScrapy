6415f9b9768404ec92f97fa4ba832f8648165ea7
Yangqi Li <jrs930805@gmail.com>
2018-06-11 00:24:53 +0800

    Final_link_crawler
    the final version

--- codes/start.py
+++ codes/start.py
@@ -3,57 +3,126 @@ import re
 import itertools
 import urlparse
 import robotparser
+import time
+from datetime import datetime
+import Queue
 
-def download(url,user_agent = 'jrs',proxy=None,num_retries = 2):
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+        
+    def wait(self, url):
+        domain = urlparse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
+
+def download(url,headers,proxy,num_retries, data = None):
     print 'Downloading:',url
-    headers = {'User-agent':user_agent}
-    request = urllib2.Request(url,headers=headers)
+    #headers = {'User-agent':user_agent}
+    request = urllib2.Request(url,data,headers)
     opener = urllib2.build_opener()
     if proxy:
         proxy_params = {urlparse.urlparse(url).scheme:proxy}
         opener.add_handler(urllib2.ProxyHandler(proxy_params))
     try:
-        html = urllib2.urlopen(request).read()
+        response = opener.open(request)
+        html = response.read()
+        code = response.code
     except urllib2.URLError as e:
         print 'Download erro:',e.reason
-        html = None
-        if num_retries>0:
-            if hasattr(e,'code') and 500<= e.code < 600:
-                return download(url,num_retries-1)
+        html = ''
+        if hasattr(e,'code'):
+            code = e.code
+            if num_retries>0 and 500<= code < 600:
+                return download(url,headers,proxy,num_retries-1,data)
+        else:
+            code = None
     return html
 
-def link_crawler(seed_url, link_regex):
+def link_crawler(seed_url, link_regex=None,delay= 5, max_depth=-1,max_urls=-1, headers=None, user_agent='jrs', proxy=None, num_retries=1):
     """crawl from the given seed URL following links matched by link_regex
     """
-    rp = robotparser.RobotFileParser()
-    rp.set_url('http://192.168.1.5:8000/places/static/robots.txt')
-    rp.read()
-    user_agent = 'BadCrawler'
-
-    crawl_queue = [seed_url]
-    seen = set(crawl_queue)
+    crawl_queue = Queue.deque([seed_url])
+    seen = {seed_url:0}
+    num_urls = 0
+    rp = get_robots(seed_url)
+    throttle = Throttle(delay)
+    headers = headers or {}
+    if user_agent:
+        headers['User-agent'] = user_agent
+    
     while crawl_queue:
         url = crawl_queue.pop()
+        # check url passes robots.txt restrictions
         if rp.can_fetch(user_agent, url):
-            html = download(url)
-            links = get_links(html)
-            for link in links:
-                if re.match(link_regex ,link):
-                    link = urlparse.urljoin(seed_url, link)
+            throttle.wait(url)
+            html = download(url, headers, proxy=proxy, num_retries=num_retries)
+            links = []
+
+            depth = seen[url]
+            if depth != max_depth:
+                # can still crawl further
+                if link_regex:
+                    # filter for links matching our regular expression
+                    links.extend(link for link in get_links(html) if re.match(link_regex, link))
+
+                for link in links:
+                    link = normalize(seed_url, link)
+                    # check whether already crawled this link
                     if link not in seen:
-                        seen.add(link)
-                        crawl_queue.append(link)
-                else:
-                    print 'do not want ',link
+                        seen[link] = depth + 1
+                        # check link is within same domain
+                        if same_domain(seed_url, link):
+                            # success! add this new link to queue
+                            crawl_queue.append(link)
+
+            # check whether have reached downloaded maximum
+            num_urls += 1
+            if num_urls == max_urls:
+                break
         else:
-            print 'Blocked by robots.txt ',url 
+            print 'Blocked by robots.txt:', url
+
+
+def normalize(seed_url, link):
+    """Normalize this URL by removing hash and adding domain
+    """
+    link, _ = urlparse.urldefrag(link) # remove hash to avoid duplicates
+    return urlparse.urljoin(seed_url, link)
+
+def same_domain(url1, url2):
+    """Return True if both URL's belong to same domain
+    """
+    return urlparse.urlparse(url1).netloc == urlparse.urlparse(url2).netloc
+
+def get_robots(url):
+    """Initialize robots parser for this domain
+    """
+    rp = robotparser.RobotFileParser()
+    rp.set_url(urlparse.urljoin(url, '/robots.txt'))
+    rp.read()
+    return rp
 
 def get_links(html):
     print 1111
     webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
     return webpage_regex.findall(html)
 
-link_crawler('http://192.168.1.5:8000/places', '/places/default/(index|view)')
+if __name__ == '__main__':
+    link_crawler('http://example.webscraping.com', '/places/default/(index|view)', delay=0, num_retries=1, user_agent='BadCrawler')
+    link_crawler('http://example.webscraping.com', '/places/default/(index|view)', delay=0, num_retries=1, max_depth=1, user_agent='GoodCrawler')
+
+#link_crawler('http://192.168.1.5:8000/places', '/places/default/(index|view)')
 '''
 rp = robotparser.RobotFileParser()
 rp.set_url('http://example.webscraping.com/robots.txt')
