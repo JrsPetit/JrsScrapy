594fe614dd5b15cb446199053a990451dc11968e
Yangqi Li <jrs930805@gmail.com>
2018-06-25 23:46:25 +0800

    warm-up
    some basic knowledge pf py

---
+++ codes/JrsPy.py
@@ -0,0 +1,31 @@
+import random as r
+
+class Fish:
+    def __init__(self):
+        self.x = r.randint(0,10)
+        self.y = r.randint(0,10)
+    
+    def move(self):
+        self.x -= 1
+        print "Now I'm at %d,%d" % (self.x,self.y)
+
+class Shark(Fish):
+    def __init__(self):
+        #Fish.__init__(self)
+        #super().__init__() #3.x
+        super(Shark,self).__init__()
+        self.hungry = True
+    
+    def eat(self):
+        if self.hungry:
+            print "happy to eat!"
+            self.hungry = False
+        else:
+            print "no more food!"
+
+if __name__ == '__main__':
+    shark = Shark()
+    shark.eat()
+    shark.eat()
+    shark.move()
+    shark.move()
