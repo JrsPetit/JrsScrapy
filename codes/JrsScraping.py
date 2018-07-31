0622a3523dbd0825427b417432be7afb4742171b
Yangqi Li <jrs930805@gmail.com>
2018-07-31 21:28:48 +0800

    use Lxml
    using re, bs4 and lxml to get data in html.

--- codes/JrsScraping.py
+++ codes/JrsScraping.py
@@ -1,5 +1,6 @@
 import re
 import urllib2
+import lxml.html
 from bs4 import BeautifulSoup
 
 def download(url,user_agent = 'jrs',num_retries = 2):
@@ -19,6 +20,7 @@ def download(url,user_agent = 'jrs',num_retries = 2):
 if __name__ == "__main__":
     url = 'http://127.0.0.1:8000/places/default/view/Aland-Islands-2'
     html = download(url)
+    
     print re.findall(r'<tr id="places_area__row">.*?<td\s*class=["\']w2p_fw["\']>(.*?)</td>',html)
     #print re.findall('<td class="w2p_fw">(.*?)</td>',html)[1]
 
@@ -26,3 +28,12 @@ if __name__ == "__main__":
     tr = soup.find(attrs={'id':'places_area__row'})
     td = tr.find(attrs={'class':'w2p_fw'})
     print td.text
+
+    broken_html = "<ul class = country><li>Area<li>Population</ul>"
+    tree = lxml.html.fromstring(broken_html)
+    fixed_html = lxml.html.tostring(tree,pretty_print=True)
+    print "new html:\n",fixed_html
+
+    tree2 = lxml.html.fromstring(html)
+    td = tree2.cssselect("tr#places_area__row > td.w2p_fw")[0]
+    print td.text_content()
