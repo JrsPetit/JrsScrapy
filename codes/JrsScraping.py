32e3cea0ac66e439060f4fdbec4f521a85cfed69
Yangqi Li <jrs930805@gmail.com>
2018-08-02 23:26:16 +0800

    three_ways
    re, bs ,lxml way to scrapy

--- codes/JrsScraping.py
+++ codes/JrsScraping.py
@@ -3,6 +3,28 @@ import urllib2
 import lxml.html
 from bs4 import BeautifulSoup
 
+FIELDS = ('area','population','iso','country','capital','continent','tld','currency_code','currency_name','phone','postal_code_format','postal_code_regex','languages','neighbours')
+
+def re_scraper(html):
+    results = {}
+    for field in FIELDS:
+        results[field] = re.search(r'<tr id="places_%s__row">.*?<td class="w2p_fw">(.*?)</td>' % field, html).groups()[0]
+    return results
+
+def bs_scraper(html):
+    soup = BeautifulSoup(html,'html.parser')
+    results = {}
+    for field in FIELDS:
+        results[field] = soup.find('table').find('tr',id='places_%s__row' % field).find('td',class='w2p_fw').text
+    return results
+
+def lxml_scraper(html):
+    tree = lxml.html.fromstring(html)
+    results = {}
+    for field in FIELDS:
+        results[field] = tree.cssselect('table>tr#places_%s__row>td.w2p_fw' % field)[0].text_content()
+    return results
+
 def download(url,user_agent = 'jrs',num_retries = 2):
     headers = {'User-agent':user_agent}
     request = urllib2.Request(url,headers=headers)
