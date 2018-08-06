d622f16a87bf0c668c788ebddc95309100c75269
Yangqi Li <jrs930805@gmail.com>
2018-08-06 23:14:57 +0800

    111
    don't want to say anything right now.

--- codes/JrsScraping.py
+++ codes/JrsScraping.py
@@ -1,11 +1,35 @@
 import re
 import urllib2
+import urlparse
+import robotparser
 import lxml.html
 from bs4 import BeautifulSoup
+from datetime import datetime
 import time
+import Queue
 
 FIELDS = ('area','population','iso','country','capital','continent','tld','currency_code','currency_name','phone','postal_code_format','postal_code_regex','languages','neighbours')
 NUM_ITERATIONS = 100 #number of times to test each scraper
+
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+        
+    def wait(self, url):
+        domain = urlparse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
+
 def re_scraper(html):
     results = {}
     for field in FIELDS:
