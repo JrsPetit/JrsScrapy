b9355aa86f3d08b29878c24680b345d08a3026cd
Yangqi Li <jrs930805@gmail.com>
2018-08-07 23:03:39 +0800

    ScrapeCallback
    how does it work?

--- codes/JrsScrapyToCSV.py
+++ codes/JrsScrapyToCSV.py
@@ -3,9 +3,25 @@ import re
 import itertools
 import urlparse
 import robotparser
+import lxml.html
 import time
 from datetime import datetime
 import Queue
+import csv
+
+class ScrapeCallback:
+    def __init__(self):
+        self.writer = csv.writer(open('countries.csv','w'))
+        self.fields = ('area','population','iso','country','capital','continent','tld','currency_code','currency_name','phone','postal_code_format','postal_code_regex','languages','neighbours')
+        self.writer.writerow(self.fields)
+    
+    def __call__(self,url,html):
+        if re.search('/view/',url):
+            tree = lxml.html.fromstring(html)
+            row = []
+            for field in self.fields:
+                row.append(tree.cssselect('table>tr#places_{}__row>td.w2p_fw'.format(field))[0].text_content())
+            self.writer.writerow(row)
 
 class Throttle:
     """Throttle downloading by sleeping between requests to same domain
