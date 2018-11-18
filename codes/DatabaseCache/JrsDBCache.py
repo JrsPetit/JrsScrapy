8ea54f138ff655385e95b7451efd31988c311b1b
Yangqi Li <jrs930805@gmail.com>
2018-11-18 23:48:14 +0800

    do with MongoDB
    do with MongoDB

--- codes/DatabaseCache/JrsDBCache.py
+++ codes/DatabaseCache/JrsDBCache.py
@@ -3,27 +3,45 @@ import os
 from pymongo import MongoClient
 from pprint import pprint
 from bson.son import SON
-'''
-class Connect(object):
+from datetime import datetime,timedelta
+
+class MongoCache:
+    def __init__(self, client=None, expires = timedelta(days=30)):
+        self.client = MongoClient('localhost',27017) if client is None else client
+        self.db = client.cache
+        self.db.webpage.create_index('timestamp',expireAfterSeconds = expires.total_seconds())
+
+    def __getitem__(self,url):
+        record = self.db.webpage.find_one({'_id':url})
+        if record:
+            return record['result']
+        else:
+            raise KeyError(url + 'does not exist')
+    
+    def __setitem__(self,url,result):
+        record = {'result':result, 'timestamp':datetime.utcnow()}
+        self.db.webpage.update({'_id':url},{'$set':record}, upsert = True)
+
+""" class Connect(object):
     def get_connection(self):
-        return MongoClient("mongodb://$[username]:$[password]@$[hostlist]/$[database]?authSource=$[authSource]")
-'''
+        return MongoClient("mongodb://$[username]:$[password]@$[hostlist]/$[database]?authSource=$[authSource]") """
+
 if __name__ == '__main__':
     client = MongoClient('localhost',27017)
     url = 'http://example.webscraping.com/view/United-kingdom-239'
     html = '...'
     db = client.cache2
-    db.webpage.insert_one(
+    """ db.webpage.insert_one(
     {
      "item": "canvas",
      "qty": 100,
      "tags": ["cotton"],
-     "size": {"h": 28, "w": 35.5, "uom": "cm"}})
+     "size": {"h": 28, "w": 35.5, "uom": "cm"}}) """
     cursor = db.webpage.find({})
     for item in cursor:
         pprint(item)
     
-    db.webpage2.insert_many([
+    """ db.webpage2.insert_many([
     {"item": "journal",
      "qty": 25,
      "size": SON([("h", 14), ("w", 21), ("uom", "cm")]),
@@ -43,11 +61,20 @@ if __name__ == '__main__':
     {"item": "postcard",
      "qty": 45,
      "size": SON([("h", 10), ("w", 15.25), ("uom", "cm")]),
-     "status": "A"}])
+     "status": "A"}]) """
     cursor2 = db.webpage2.find({"status":"D"})
     cursor3 = db.webpage2.find({"size.uom":"in"})
-
-    for item in cursor2:
+    cursor4 = db.webpage2.find({"size.h":{"$lt":9}})#less than/greater than
+    cursor5 = db.webpage2.find({"status":"A","size.h":{"$lt":9}})
+    #cursor5 = db.webpage2.find({"$and":[{"status":"A"},{"size.h":{"$lt":9}}]})
+    cursor6 = db.webpage2.find({"$or":[{"status":"A"},{"size.h":{"$lt":9}}]})
+    cursor7 = db.webpage2.find({
+        "status":"A",
+        "$or":[{"qty":{"$lt":"40"}},{"item":{"$regex":"^p"}}]
+    })
+    print "----cursor4----"
+    for item in cursor6:
         pprint(item)
-    for item in cursor3:
+    print "----cursor5----"
+    for item in cursor7:
         pprint(item)
