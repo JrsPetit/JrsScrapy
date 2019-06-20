3bfe127c157d56ceddf8986c9cbbae36ae883cb4
Yangqi Li <jrs930805@gmail.com>
2019-06-20 22:51:25 +0800

    again

--- codes/JrsImage.py
+++ codes/JrsImage.py
@@ -2,6 +2,6 @@ from skimage import io
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    img = io.imread('test.jpg')
+    img = io.imread("E:\JrsGithub\JrsScrapy\codes\\1212.jpg")
     io.imshow(img)
     plt.show()
