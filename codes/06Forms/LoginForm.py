18b7c8f1755c1388f11028445fed6e468e0e63c9
Yangqi Li <jrs930805@gmail.com>
2020-05-22 18:49:36 +0800

    Scrapy
    Do with Scrapy

--- codes/06Forms/LoginForm.py
+++ codes/06Forms/LoginForm.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import urllib
 import urllib2
+import random
 
 if __name__ == "__main__":
     LOGIN_URL = 'http://example.webscraping.com/user/login'
-    
+    for i in range(1,10):
+        print random.randint(1,10)
---
+++ codes/JrsScrapy/JrsScrapy/spiders/country.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from JrsScrapy.items import JrsscrapyItem
+
+class CountrySpider(CrawlSpider):
+    name = 'country'
+    allowed_domains = ['example.webscraping.com']
+    start_urls = ['http://example.webscraping.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow='/index/',deny='/user/'),follow=True),
+        Rule(LinkExtractor(allow='/view/',deny='/user/'),callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        item = JrsscrapyItem()
+        name_css = 'tr#places_country__row td.w2p_fw::text'
+        item['name'] = response.css(name_css).extract()
+        pop_css = 'tr#places_population__row td.w2p_fw::text'
+        item['population'] = response.css(pop_css).extract()
+        return item
